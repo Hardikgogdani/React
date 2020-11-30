9597529034d3cebf78c0b76ffcc14afb10383c55
hardik gogdani <hardikgogdani989@gmail.com>
2020-11-30 14:01:12 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
+
+let list1 = [];
 export default class App extends Component {
 
     state = {
@@ -25,6 +27,18 @@ export default class App extends Component {
             hobbies: ""
         }
     };
+    componentDidMount() {
+
+        list1 = [];
+        if(JSON.parse(localStorage.getItem("list")) !== null ) {
+            list1 = JSON.parse(localStorage.getItem("list"));
+
+        };
+        this.setState({list: list1});
+
+    }
+
+
     setGender = (e) => {
         this.setState({
             gender: e.target.value
@@ -50,7 +64,74 @@ export default class App extends Component {
         this.setState(payload);
 
     };
-    handleReset = () => {
+
+    validation = () => {
+        const { firstName, lastName, email, address, gender, hobbies, color, phonenumber } = this.state;
+        const error = {};
+
+        if (firstName === "") {
+            error.firstName = "Enter the first name!";
+        }
+        if (lastName === "") {
+            error.lastName = "Enter the last name!";
+        }
+        if (!email.includes("@")) {
+            error.email = "Enter the Valid E-mail!";
+        }
+        if (address === "") {
+            error.address = "Enter the address!";
+        }
+        if (gender === "") {
+            error.gender = "select the Gender!";
+        }
+        if (hobbies === "") {
+            error.hobbies = "check your Hobbies!";
+        }
+        if (color === "") {
+            error.color = "Enter your favroute Color!";
+        }
+        if (phonenumber === "") {
+            error.phonenumber = "Enter your Phonenumber!";
+        }
+        this.setState({ error })
+        
+    }
+
+    handleSubmit = () => {
+        const x = this.validation();
+        const { firstName, lastName, email, gender, address, color, hobbies, phonenumber, list } = this.state
+
+        // if(!localStorage.getItem('list')){
+        //     list.push({
+        //         firstName: firstName,
+        //         lastName: lastName,
+        //         email: email,
+        //         address: address,
+        //         gender: gender,
+        //         hobbies: hobbies,
+        //         color: color,
+        //         phonenumber: phonenumber
+        //     });
+        //     localStorage.setItem('list',JSON.stringify(list));
+        // }
+        // else{
+            if(!x) {
+                if(!localStorage.getItem('list')){
+                    list.push({
+                        firstName: firstName,
+                        lastName: lastName,
+                        email: email,
+                        address: address,
+                        gender: gender,
+                        hobbies: hobbies,
+                        color: color,
+                        phonenumber: phonenumber
+                    });
+                } 
+                this.setState({ list })
+                localStorage.setItem('list',JSON.stringify(list));
+            }
+        // }
         this.setState({
             firstName: '',
             lastName: "",
@@ -60,117 +141,21 @@ export default class App extends Component {
             color: "",
             phonenumber: "",
             hobbies: "",
-            isEditableIndex: null
         });
-    };
-
-    // isValidate = () => {
-    //     const isValid = this.validation();
-    //     if (isValid) {
-    //         console.log(this.state);
-    //     }
-    // };
-
-    validation = () => {
-        const { firstName, lastName, email, address, gender, hobbies, color, phonenumber } = this.state;
-
-        if (!this.state.firstName) {
-            this.setState({ error: { ...this.state.error , firstName: "Enter the first name!" } })
-        }
-        if (!this.state.lastName) {
-            this.setState({ error: { ...this.state.error , lastName: "Enter the last name!" } })
-        }
-        if (!this.state.email.includes("@")) {
-            this.setState({ error: {  ...this.state.error , email: "Enter the valid email!" } })
-            
-        }
-        if (!this.state.address) {
-            this.setState({ error: {  ...this.state.error , address: "Enter the address!" } })
-            
-        }
-        if (!this.state.gender) {
-            this.setState({ error: { ...this.state.error , gender: "Enter the gender!" } })
-            
-        }
-        if (!this.state.hobbies) {
-            this.setState({ error: {  ...this.state.error ,hobbies: "Enter the hobbies!" } })
-            
-        }
-        if (!this.state.color) {
-            this.setState({ error: {  ...this.state.error , color: "Enter the color!" } })
-            
-        }
-        if (!this.state.phonenumber) {
-            this.setState({ error: {  ...this.state.error , phonenumber: "Enter the phonenumber!" } })
-            
-        }
-
-        return true;
-        const validState = {
-            lblFirstName: false,
-            lblLastName: false,
-            lblAddress: false,
-            lblGender: false,
-            lblEmail: false,
-            lblHobbies: false,
-            lblColor: false,
-            lblPhonenumber: false
-        }
-        if (!firstName) {
-            ValidityState.lblFirstName = true;
-        }
-        if (!lastName) {
-            ValidityState.lblLastName = true;
-        }
-        if (!email) {
-            ValidityState.lblEmail = true;
-        }
-        if (!address) {
-            ValidityState.lblAddress = true;
-        }
-        if (!gender) {
-            ValidityState.lblGender = true;
-        }
-        if (!hobbies) {
-            ValidityState.lblHobbies = true;
-        }
-        if (!color) {
-            ValidityState.lblColor = true;
-        }
-        if (!phonenumber) {
-            ValidityState.lblPhonenumber = true;
-        }
-        this.setState(ValidityState);
     }
 
-    handleSubmit = () => {
-        const x = this.validation();
-        if (x) {
-            const { firstName, lastName, email, gender, address, color, hobbies, phonenumber, list } = this.state
-            console.log(this.state);
-            list.push({
-                firstName: firstName,
-                lastName: lastName,
-                email: email,
-                address: address,
-                gender: gender,
-                hobbies: hobbies,
-                color: color,
-                phonenumber: phonenumber
-            });
-            this.setState({ list })
-            console.log(this.state)
-
-        }
-    }
 
     deletee = (index) => {
+        list1 = JSON.parse(localStorage.getItem('list'));
         const { list } = this.state
         list.splice(index, 1);
         this.setState({ list })
+        localStorage.removeItem('list');
+        
     }
 
     updatee = (index) => {
+
         const { list } = this.state
         const update = {
             firstName: list[index].firstName,
@@ -183,13 +168,13 @@ export default class App extends Component {
             phonenumber: list[index].phonenumber,
         };
 
-        this.setState({ isEditableIndex: index, ...update})
+        this.setState({ isEditableIndex: index, ...update })
     }
     render() {
         return (
             <React.Fragment>
                 <h1>Registration form</h1>
-        First name:<input type="text" value={this.state.firstName} name="firstName" onChange={this.handleChange} /><span style={{ color: "red" }}>{this.state.error.firstName}</span><br /><br />
+                    First name:<input type="text" value={this.state.firstName} name="firstName" onChange={this.handleChange} /><span style={{ color: "red" }}>{this.state.error.firstName}</span><br /><br />
 
                     Last name:<input type="text" value={this.state.lastName} name="lastName" onChange={this.handleChange} /><span style={{ color: "red" }}>{this.state.error.lastName}</span><br /><br />
 
@@ -198,7 +183,7 @@ export default class App extends Component {
                     Address:<textarea type="text" value={this.state.address} name="address" onChange={this.handleChange} maxLength="50" cols="45" rows="2" /><span style={{ color: "red" }}>{this.state.error.address}</span><br /><br />
 
                     Gender:<div onChange={this.setGender}>
-                    <input type="radio" value="male" checked={this.state.gender === "male"} name="gender" checked />Male
+                    <input type="radio" value="male" checked={this.state.gender === "male"} name="gender" />Male
                 <input type="radio" value="female" checked={this.state.gender === "female"} name="gender" />Female<span style={{ color: "red" }}>{this.state.error.gender}</span><br /><br />
                 </div>
 
@@ -210,20 +195,17 @@ export default class App extends Component {
                     <input type="checkbox" name="hobbies" checked={this.state.hobbies.includes("Dancing")} value="Dancing" />Dancing <span style={{ color: "red" }}>{this.state.error.hobbies}</span><br /><br />
                 </div>
 
-                    Favroute Color:<select value={this.state.color} name="color" onChange={this.handleChange}><span style={{ color: "red" }}>{this.state.error.color}</span>
+                    Favroute Color:<select value={this.state.color} name="color" onChange={this.handleChange}>
                     <option value="Red" onChange={this.handleChange}>Red</option>
                     <option value="green" onChange={this.handleChange}>Green</option>
                     <option value="Black" onChange={this.handleChange} >Black</option>
                     <option value="lightred" onChange={this.handleChange}>lightred</option>
                     <option value="lightgreen" onChange={this.handleChange}>lightgreen</option>
-                </select><br /><br />
+                </select><span style={{ color: "red" }}>{this.state.error.color}</span><br /><br />
 
                     Phone Number : <input type="tel" maxlength="10" value={this.state.phonenumber} onChange={this.handleChange} name="phonenumber" /> <span style={{ color: "red" }}>{this.state.error.phonenumber}</span><br /><br />
 
                 <button onClick={this.handleSubmit}>Submit</button>
-
-                <button onClick={this.handleReset}>Reset</button><br /><br />
-
 
                 <table border="1px solid">
                     <thead>
