437727d4255658067fbb20c3299000dc0c0df974
hardik gogdani <hardikgogdani989@gmail.com>
2020-12-02 11:43:08 +0530

    hiding and showing of table

--- src/hook.js
+++ src/hook.js
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Hook = () => {
-    const state = {
-        firstName: "",
-        lastName: "",
-        age: "",
-        gender: "",
-        address: "",
-        country: "",
-        isActive: ""
+
+    const [userDetail, setUserDetail] = useState({});
+    const [data, setData] = useState([]);
+
+    // here hiding and showing of table
+    const [showResults, setShowResults] = React.useState(true)
+    const onClick = () => setShowResults(!showResults)
+
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setUserDetail({ ...userDetail, [name]: value })
     }
+
+    const SubmitValue = () => {
+        setShowResults(!showResults)
+        setData([...data, userDetail])
+
+        setUserDetail({})
+        
+    }
+
+
     return (
         //creation of form without form attribute is here.
         <>
             <div className="container">
-                <button className="btn-primary" type="button" data-target="#hideen" data-toggle="collapse" aria-expanded="false" aria-controls="collapseExample">Add New</button>
+
+                <button className="btn-primary" type="button" onClick={onClick} value="Add Me">Add Me </button>
+
                 <div className="row">
                     <div className="table-responsive">
                         <table className="table">
@@ -33,25 +49,26 @@ const Hook = () => {
                         </table><br />
                     </div>
                 </div>
-                <div className="row>">
+
+                {showResults  && <div className="row>">
 
                     <div className="col-md-6" id="hideen">
                         <h2>Registration Form</h2> <br />
                 First name: <br />
-                        <input className="form-control" name="firstname" type="text" placeholder="Enter YourFirst Name" /><br />
+                        <input className="form-control" name="firstname" type="text" value={userDetail.firstName || ''} onChange={handleChange} /><br />
                 Last name: <br />
-                        <input className="form-control" name="lastname" type="text" placeholder="Enter YourLast Name" /><br />
+                        <input className="form-control" name="lastname" type="text" value={userDetail.lastName || ''} onChange={handleChange} /><br />
                 Age :<br />
-                        <input className="form-control" type="text" name="age" placeholder="Enter Your Age" /><br />
+                        <input className="form-control" type="text" name="age" value={userDetail.age || ''} onChange={handleChange} /><br />
                 Gender : <br />
-                        <input type="radio" name="gender" value="male" />Male
-                <input type="radio" name="gender" value="female" />Female
-                <input type="radio" name="gender" value="others" />Others<br /><br />
+                        <input type="radio" name="gender" value="male" onChange={handleChange} value="male" />Male
+                <input type="radio" name="gender" value="female" onChange={handleChange} value="female" />Female
+                <input type="radio" name="gender" value="others" onChange={handleChange} value="other" />Others<br /><br />
                 Address : <br />
-                        <textarea className="form-control" cols="25" rows="4" placeholder="Enter Your Address">
+                        <textarea className="form-control" cols="25" rows="4" placeholder="Enter Your Address" value={userDetail.address || ''} onChange={handleChange}>
                         </textarea>
                 Country : <br />
-                        <select className="form-control" name="country" id="country" required>
+                        <select className="form-control" name="country" id="country" value={userDetail.country || ''} onChange={handleChange} required>
                             <option>Select Your Country</option>
                             <option value="India">India</option>
                             <option value="Brazil">Brazil</option>
@@ -59,10 +76,10 @@ const Hook = () => {
                             <option value="UK">UK</option>
                             <option value="Dubai">Dubai</option>
                         </select><br />
-                        <input type="checkbox" /> <label>I Agree the Terms & Conditions</label><br />
-                        <button className="btn-primary">Submit</button>
+                        IsActive: <input type="checkbox" name="active" onChange={handleChange} /><br />
+                        <button className="btn-primary" onClick={SubmitValue}>Submit</button>
                     </div>
-                </div>
+                </div>}
             </div>
 
         </>
