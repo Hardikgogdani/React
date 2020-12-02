a72a548a4fbac10a62ab4e4109b0a29487c3f2f7
hardik gogdani <hardikgogdani989@gmail.com>
2020-12-02 12:10:42 +0530

    change

--- src/hook.js
+++ src/hook.js
@@ -1,88 +1,99 @@
-import React, { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useState} from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
 
 const Hook = () => {
-
     const [userDetail, setUserDetail] = useState({});
-    const [data, setData] = useState([]);
-
-    // here hiding and showing of table
+    const [list, setList] = useState([]);
+    
     const [showResults, setShowResults] = React.useState(true)
-    const onClick = () => setShowResults(!showResults)
 
+    const onAdd = () => {
+        setShowResults(!showResults)
+    }
 
     const handleChange = e => {
-        const { name, value } = e.target;
-        setUserDetail({ ...userDetail, [name]: value })
+        const {name, value,checked} = e.target;
+        if(name === "active" ){
+            setUserDetail({...userDetail, [name]: checked})
+        }else{
+            setUserDetail({...userDetail, [name]: value})
+        }
+
     }
 
-    const SubmitValue = () => {
+    const onDelete = (index) => {
+        setList(list.filter((value,i) => i !== index))
+    }
+
+    const onEdit = () => {
+
+    }
+
+    const submitValue = () => {
         setShowResults(!showResults)
-        setData([...data, userDetail])
-
+        setList([...list, userDetail])
         setUserDetail({})
-        
     }
-
 
     return (
-        //creation of form without form attribute is here.
-        <>
-            <div className="container">
+        <div className="container">
+            <br /><button className="btn-primary" type="button" onClick={onAdd}>Add New</button><br /><br />
+            <div className="row table-responsive">
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Age</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        list.map((value, index) => (
+                            <tr key={index}>
+                                <td>{value.firstName}</td>
+                                <td>{value.lastName}</td>
+                                <td>{value.age}</td>
+                                <td>
+                                    <button onClick={()=>{onEdit(index)}}>Edit</button>
+                                    <button onClick={()=>onDelete(index)}>Delete</button>
+                                </td>
 
-                <button className="btn-primary" type="button" onClick={onClick} value="Add Me">Add Me </button>
-
-                <div className="row">
-                    <div className="table-responsive">
-                        <table className="table">
-                            <thead>
-                                <tr className="table-striped">
-                                    <th>First name</th>
-                                    <th>Last name</th>
-                                    <th>Age</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-
-                            </tbody>
-                        </table><br />
-                    </div>
-                </div>
-
-                {showResults  && <div className="row>">
-
-                    <div className="col-md-6" id="hideen">
-                        <h2>Registration Form</h2> <br />
-                First name: <br />
-                        <input className="form-control" name="firstname" type="text" value={userDetail.firstName || ''} onChange={handleChange} /><br />
-                Last name: <br />
-                        <input className="form-control" name="lastname" type="text" value={userDetail.lastName || ''} onChange={handleChange} /><br />
-                Age :<br />
-                        <input className="form-control" type="text" name="age" value={userDetail.age || ''} onChange={handleChange} /><br />
-                Gender : <br />
-                        <input type="radio" name="gender" value="male" onChange={handleChange} value="male" />Male
-                <input type="radio" name="gender" value="female" onChange={handleChange} value="female" />Female
-                <input type="radio" name="gender" value="others" onChange={handleChange} value="other" />Others<br /><br />
-                Address : <br />
-                        <textarea className="form-control" cols="25" rows="4" placeholder="Enter Your Address" value={userDetail.address || ''} onChange={handleChange}>
-                        </textarea>
-                Country : <br />
-                        <select className="form-control" name="country" id="country" value={userDetail.country || ''} onChange={handleChange} required>
-                            <option>Select Your Country</option>
-                            <option value="India">India</option>
-                            <option value="Brazil">Brazil</option>
-                            <option value="USA">USA</option>
-                            <option value="UK">UK</option>
-                            <option value="Dubai">Dubai</option>
-                        </select><br />
-                        IsActive: <input type="checkbox" name="active" onChange={handleChange} /><br />
-                        <button className="btn-primary" onClick={SubmitValue}>Submit</button>
-                    </div>
-                </div>}
+                            </tr>
+                        ))
+                    }
+                    </tbody>
+                </table>
             </div>
+            <br/>
+            {showResults && <div className="col-md-6">
+                <h2>Registration Form</h2><br />
 
-        </>
+                <b>FIRST NAME</b> : <input type="text" name="firstName" value={userDetail.firstName || ''}
+                                           onChange={handleChange}/><br/><br/>
+                <b>LAST NAME</b> : <input type="text" name="lastName" value={userDetail.lastName || ''}
+                                          onChange={handleChange}/><br/><br/>
+                <b>AGE</b> : <input type="text" name="age" value={userDetail.age || ''}
+                                    onChange={handleChange}/><br/><br/>
+                <b>GENDER</b> :{' '}<input type="radio" name="gender" checked={userDetail.gender === "male"} onChange={handleChange} value="male"/>Male{' '}
+                <input type="radio" name="gender" checked={userDetail.gender === "female"} onChange={handleChange} value="female"/>Female{' '}
+                <input type="radio" name="gender" checked={userDetail.gender === "other"} onChange={handleChange} value="other"/>Other<br/><br/>
+                <b>ADDRESS</b> : <input type="text" name="address" value={userDetail.address || ''}
+                                        onChange={handleChange}/><br/><br/>
+                <b>COUNTRY</b>:{' '}<select name="country" value={userDetail.country || ''} onChange={handleChange}>
+                <option value="India">India</option>
+                <option value="Brazil">Brazil</option>
+                <option value="USA">USA</option>
+                <option value="Dubai">Dubai</option>
+                <option value="UK">UK</option>
+            </select><br/><br/>
+                <b>I Agree the Terms & Conditions:</b>:{' '} <input type="checkbox" checked={userDetail.active} name="active" onChange={handleChange}/><br/><br/>
+
+                <button  className="btn-primary" onClick={submitValue}>Submit</button>
+            </div>}
+        </div>
     )
 }
 export default Hook;
